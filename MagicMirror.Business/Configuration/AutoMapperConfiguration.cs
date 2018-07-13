b443ea808bd66a5e121d1189f712ed1f390e5de0
Michiel Wouters <michiel_woute64@hotmail.com>
2018-07-14 00:19:55 +0200

    Dialed back UserInfo

--- MagicMirror.Business/Configuration/AutoMapperConfiguration.cs
+++ MagicMirror.Business/Configuration/AutoMapperConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using MagicMirror.Business.Models;
-using MagicMirror.DataAccess.Entities.Entities;
 using MagicMirror.DataAccess.Entities.Traffic;
 using MagicMirror.DataAccess.Entities.Weather;
 
@@ -22,9 +21,6 @@ namespace MagicMirror.Business.Configuration
                 .ForMember(x => x.Origin, y => y.MapFrom(z => z.Origin_addresses[0]))
                 .ForMember(x => x.Distance, y => y.MapFrom(z => z.Rows[0].Elements[0].Distance.Value))
                 .ForMember(x => x.Duration, y => y.MapFrom(z => z.Rows[0].Elements[0].Duration.Value));
-
-
-            CreateMap<UserEntity, UserInformation>();
         }
     }
 }
